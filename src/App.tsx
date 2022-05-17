90dde449d3bb88ae90f083919ab0c0cc377d2785
KIMKICHAN <rlcks980705@gmail.com>
2022-05-17 18:49:38 +0900

    refactor : 컴포넌트 구성을 헤더와 영화 리스트들을 담고 있는 컨테이너로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,6 @@ import { getMovies } from "./model/database";
 import { moviesDummy1, moviesForm } from "./model/movies";
 
 function App() {
-  let settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 6,
-  };
-  const [sliderSettings, setSliderSettings] = useState({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 0,
-    slidesToScroll: 0,
-  });
   return (
     <>
       <Header />
