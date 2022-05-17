04b17874f89e00c9f9e8625d44553a67c82697cf
KIMKICHAN <rlcks980705@gmail.com>
2022-05-17 18:28:15 +0900

    refactor : 로컬 스토리지에서 카테고리에 따른 영화 데이터 로드

--- src/model/database.ts
+++ src/model/database.ts
@@ -3,5 +3,3 @@ import { movieData } from "./interface";
 export const getMovies = (categoryTitle: string): movieData[] => {
   return JSON.parse(localStorage.getItem(categoryTitle) || "");
 };
-
-export const top2030Movies = getMovies("Top250Movies").slice(20, 30);
