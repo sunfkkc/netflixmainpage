1e210b0dbbb72465c197b31d92ceb4353790f51b
KIMKICHAN <rlcks980705@gmail.com>
2022-05-17 18:46:32 +0900

    refactor : 카테고리 타이틀을 URL이라고 가정

--- src/model/movies.ts
+++ src/model/movies.ts
@@ -1,37 +1,30 @@
-import { getMovies } from "./database";
-
 const REACT_APP_IMDB_API_KEY = process.env.REACT_APP_IMDB_API_KEY;
+
 export interface moviesForm {
   id: number;
   categoryTitle: string;
-  url: string;
 }
 
 export const moviesDummy1: moviesForm[] = [
   {
     id: 1,
-    categoryTitle: "Top250Movies",
-    url: `https://imdb-api.com/en/API/Top250Movies/${REACT_APP_IMDB_API_KEY}`,
+    categoryTitle: "Top250Movies", //API를 요청하는 URL
   },
   {
     id: 2,
     categoryTitle: "MostPopularMovies",
-    url: `https://imdb-api.com/en/API/MostPopularMovies/${REACT_APP_IMDB_API_KEY}`,
   },
   {
     id: 3,
     categoryTitle: "BoxOffice",
-    url: `https://imdb-api.com/en/API/BoxOffice/${REACT_APP_IMDB_API_KEY}`,
   },
   {
     id: 4,
     categoryTitle: "Top250TVs",
-    url: `https://imdb-api.com/en/API/Top250TVs/${REACT_APP_IMDB_API_KEY}`,
   },
   {
     id: 5,
     categoryTitle: "MostPopularTVs",
-    url: `https://imdb-api.com/en/API/MostPopularTVs/${REACT_APP_IMDB_API_KEY}`,
   },
 ];
 
@@ -39,6 +32,5 @@ export const headMovies: moviesForm[] = [
   {
     id: 1,
     categoryTitle: "Top250Movies",
-    url: `https://imdb-api.com/en/API/Top250Movies/${REACT_APP_IMDB_API_KEY}`,
   },
 ];
