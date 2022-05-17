29ed05f76f3dcd9c400218a3d07b4e3482c8f549
KIMKICHAN <rlcks980705@gmail.com>
2022-05-17 18:36:17 +0900

    refactor : 로컬 스토리지를 API 요청으로 가정

--- src/model/database.ts
+++ src/model/database.ts
@@ -3,3 +3,5 @@ import { movieData } from "./interface";
 export const getMovies = (categoryTitle: string): movieData[] => {
   return JSON.parse(localStorage.getItem(categoryTitle) || "");
 };
+
+//로컬 스토리지를 API 요청으로 가정
