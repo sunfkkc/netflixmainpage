c2cc426a6eaa0b04334395192d7bac4076dd7c9c
KIMKICHAN <rlcks980705@gmail.com>
2022-05-18 15:50:32 +0900

    refactor : movies 데이터 로드 후 slider 생성

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -6,38 +6,17 @@ import MovieItem from "./MovieItem";
 import Slider from "react-slick";
 
 interface Props {
-  url: string;
   categoryTitle: string;
 }
 
-function MoviesList({ url, categoryTitle }: Props) {
-  const [movies, setMovies] = useState(getMovies(categoryTitle));
-  const [numberOfItems, setNumberOfItems] = useState(6);
-  /* useEffect(() => {
-    const getWindowSize = () => {
-      const windowWidth = window.innerWidth;
-      const imgWidth = 200;
-      if (windowWidth >= 5 * imgWidth) {
-        setNumberOfItems(5);
-      } else if (windowWidth >= 4 * imgWidth) {
-        setNumberOfItems(4);
-      } else if (windowWidth >= 3 * imgWidth) {
-        setNumberOfItems(3);
-      } else if (windowWidth >= 2 * imgWidth) {
-        setNumberOfItems(2);
-      } else {
-        setNumberOfItems(1);
-      }
-    };
-
-    window.addEventListener("resize", getWindowSize);
-  }, []); */
+function MoviesList({ categoryTitle }: Props) {
+  const movies = getMovies(categoryTitle);
   let settings = {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: numberOfItems,
-    slidesToScroll: numberOfItems,
+    slidesToShow: 6,
+    slidesToScroll: 6,
   };
   return (
     <>
