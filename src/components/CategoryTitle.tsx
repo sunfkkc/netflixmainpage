4f3f888606f31e39399071ded0a86c5ce9d37b63
KIMKICHAN <rlcks980705@gmail.com>
2022-05-18 15:47:37 +0900

    style

--- src/components/CategoryTitle.tsx
+++ src/components/CategoryTitle.tsx
@@ -3,7 +3,7 @@ interface Props {
   categoryTitle: string;
 }
 function CategoryTitle({ categoryTitle }: Props) {
-  return <h2 style={{ color: "white" }}>{categoryTitle}</h2>;
+  return <h2 style={{ color: "black" }}>{categoryTitle}</h2>;
 }
 
 export default CategoryTitle;
